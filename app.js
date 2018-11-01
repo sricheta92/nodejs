57f2381dbf8dcd31a0365b3f3c26e2abbaf285a1
Sricheta Ruj <srichetaruj.iste@gmail.com>
2018-11-01 11:45:52 -0700

    updating connection string

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ var db_user = "cmpe281" ;
 var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
-
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
+//mongodb://<dbuser>:<dbpassword>@ds249123.mlab.com:49123/testgumball
+var db_host = (process.env.mongodb_host || "ds249123.mlab.com" ) ;
+var db_port = (process.env.mongodb_port || "49123" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "testgumball" ) ;
    
 
 var db = new DB(db_name,
